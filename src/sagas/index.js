7e4a2a0163c667a178f482bba9ade9d27f67ea62
Jacob Hsu <jacob.hsu.tw@gmail.com>
2020-09-12 15:17:24 +0800

    clear

--- src/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import { fork, all } from 'redux-saga/effects';
-
-import { watchMarket } from './exchangeActions';
-import { watchUser } from './userActions';
-
-export default function* root() {
-  yield all([fork(watchMarket), fork(watchUser)]);
-}
